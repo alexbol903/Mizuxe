fbe9a16d9fe284597ffb0de096e5d0932d964d51
alexbol903 <alexbol903@gmail.com>
2019-05-10 21:17:22 +0300

    add animlp library

---
+++ js/index.js
@@ -0,0 +1,156 @@
+animLP({
+  elements: [
+    {
+      class: 'navbar-brand',
+      duration: 800,
+      delay: 100,
+      animation: 'rubberBand'
+    },
+    {
+      class: 'slide-right-1',
+      duration: 800,
+      delay: 100,
+      animation: 'bounceLeft'
+    },
+    {
+      class: 'slide-right-2',
+      duration: 800,
+      delay: 300,
+      animation: 'bounceLeft'
+    },
+    {
+      class: 'slide-right-3',
+      duration: 800,
+      delay: 500,
+      animation: 'bounceLeft'
+    },
+    {
+      class: 'fade-poster',
+      duration: 1000,
+      delay: 1000,
+      animation: 'fade'
+    },
+    {
+      class: 'menu-1',
+      duration: 1000,
+      delay: 500,
+      animation: 'fadeDown'
+    },
+    {
+      class: 'menu-2',
+      duration: 1000,
+      delay: 800,
+      animation: 'fadeDown'
+    },
+    {
+      class: 'menu-3',
+      duration: 1000,
+      delay: 1100,
+      animation: 'fadeDown'
+    },
+    {
+      class: 'menu-4',
+      duration: 1000,
+      delay: 1400,
+      animation: 'fadeDown'
+    },
+    {
+      class: 'input-1',
+      duration: 1000,
+      delay: 1400,
+      animation: 'fadeLeft'
+    },
+    {
+      class: 'input-2',
+      duration: 1000,
+      delay: 1700,
+      animation: 'fadeLeft'
+    },
+    {
+      class: 'input-3',
+      duration: 1000,
+      delay: 2000,
+      animation: 'fadeLeft'
+    },
+    {
+      class: 'card-1',
+      duration: 1000,
+      delay: 200,
+      animation: 'flipY'
+    },
+    {
+      class: 'card-2',
+      duration: 1000,
+      delay: 400,
+      animation: 'flipY'
+    },
+    {
+      class: 'card-3',
+      duration: 1000,
+      delay: 600,
+      animation: 'flipY'
+    },
+    {
+      class: 'card-4',
+      duration: 1000,
+      delay: 800,
+      animation: 'flipY'
+    },
+    {
+      class: 'title',
+      duration: 800,
+      animation: 'zoomDown'
+    },
+    {
+      class: 'logo-footer',
+      duration: 1000,
+      animation: 'flipX'
+    },
+    {
+      class: 'input-2-1',
+      duration: 1000,
+      delay: 100,
+      animation: 'fadeUp'
+    },
+    {
+      class: 'input-2-2',
+      duration: 1000,
+      delay: 300,
+      animation: 'fadeUp'
+    },
+    {
+      class: 'input-2-3',
+      duration: 1000,
+      delay: 600,
+      animation: 'fadeUp'
+    },
+    {
+      class: 'input-2-4',
+      duration: 1000,
+      delay: 900,
+      animation: 'fadeUp'
+    },
+    {
+      class: 'accord-1',
+      duration: 1000,
+      delay: 200,
+      animation: 'bounceLeft'
+    },
+    {
+      class: 'accord-2',
+      duration: 1000,
+      delay: 500,
+      animation: 'bounceRight'
+    },
+    {
+      class: 'accord-3',
+      duration: 1000,
+      delay: 800,
+      animation: 'bounceLeft'
+    },
+  ],
+  param: {
+    startAnim: 100,
+    once: false
+  }
+});
